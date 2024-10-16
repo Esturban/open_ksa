0d6bd66ff2184c042e6e5ab1bcddf37f5942fafa
Esturban <info@evadvisory.ca>
2024-10-16 16:30:24 +0000

    Update versioning
    Using the versioning of the SCM package to effectively maintain the state of the package

--- open_ksa/__init__.py
+++ open_ksa/__init__.py
@@ -1,5 +1,7 @@
-#__version__ = "0.1.0"  # Add this line with your desired version
+# open_ksa/__init__.py
+from setuptools_scm import get_version
 
+__version__ = get_version()
 from urllib.parse import urlparse, quote
 from .download_file import download_file
 from .get_dataset_resource import get_dataset_resource
