9a6925ab10098c109fbfe1138df3005e0f74a1f3
Esturban <info@evadvisory.ca>
2024-10-16 15:20:16 +0300

    Dataset Resource
    Enabling single resource download

--- open_ksa/__init__.py
+++ open_ksa/__init__.py
@@ -2,6 +2,7 @@
 
 from urllib.parse import urlparse, quote
 from .download_file import download_file
+from .get_dataset_resource import get_dataset_resource
 from .get_dataset_resources import get_dataset_resources
 from .get_org_resources import get_org_resources
 from .ssl_adapter import SSLAdapter, SingletonSession
