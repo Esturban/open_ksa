cdf771e465ccb70cb1959b12e1edbf88948e8d93
Esturban <info@evadvisory.ca>
2024-10-23 11:23:49 +0300

    Parallel Resources
    Enabling the ability to download in parallel based on the number passed
    
    If the value doesn't exist, then a regular for loop is run
    
    Max workers currently has no limitation
    
    Consider forcing a stop in the runtime if max_workers exceed max available

--- open_ksa/get_dataset_resources.py
+++ open_ksa/get_dataset_resources.py
@@ -1,7 +1,7 @@
 
 import os
 import requests
-
+import concurrent.futures
 import time
 
 from .download_file import download_file
@@ -10,7 +10,7 @@ from urllib.parse import urlparse, quote
 from .ssl_adapter import SingletonSession
 
 
-def get_dataset_resources(dataset_ids,allowed_exts=['csv', 'xlsx', 'xls'],output_dir=f"opendata/org_resources",verbose = False):
+def get_dataset_resources(dataset_ids,allowed_exts=['csv', 'xlsx', 'xls'],output_dir=f"opendata/org_resources",verbose = None, ext_dir=None, max_workers=None):
     """Download the resources for each dataset in the list of dataset IDs
 
     Args:
@@ -18,6 +18,11 @@ def get_dataset_resources(dataset_ids,allowed_exts=['csv', 'xlsx', 'xls'],output
         allowed_exts (list, optional): The list of allowed file extensions to try to download. Defaults to ['csv', 'xlsx', 'xls'].
         output_dir (str, optional): The directory to save the downloaded files. Defaults to f"opendata/org_resources".
         verbose (bool, optional): Whether to print verbose output. Defaults to False.
+        ext_dir (bool, optional): The directory to save the downloaded files. Defaults to None.
+        max_workers (int, optional): The maximum number of workers to use for the concurrent download. Defaults to None.
+
+    Returns:
+        None: No value returned. Files downloaded to specified directory in `output_dir`
     """
     session = SingletonSession.get_instance()
     headers = {
@@ -29,7 +34,15 @@ def get_dataset_resources(dataset_ids,allowed_exts=['csv', 'xlsx', 'xls'],output
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
+        # Function to download a single dataset resource
+    def download_resource(dataset_id):
+        get_dataset_resource(dataset_id=dataset_id, allowed_exts=allowed_exts, output_dir=output_dir, headers=headers, ext_dir=ext_dir, verbose=verbose)
 
     # Download each dataset and save it to the directory
-    for dataset_id in dataset_ids: get_dataset_resource(dataset_id,allowed_exts,output_dir,headers,verbose)
+
+    if max_workers:
+         with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            executor.map(download_resource, dataset_ids)
+    else:
+        for dataset_id in dataset_ids: download_resource(dataset_id = dataset_id)
         
