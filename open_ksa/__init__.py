661621adcaa0eb5ee4a438614b9770af253a0ae9
Esturban <info@evadvisory.ca>
2024-10-23 11:18:28 +0300

    Version Fallback
    Including a fallback for the SCM versions

--- open_ksa/__init__.py
+++ open_ksa/__init__.py
@@ -1,7 +1,13 @@
 # open_ksa/__init__.py
 from setuptools_scm import get_version
 
-__version__ = get_version()
+try:
+    # Attempt to get the version from the SCM metadata
+    __version__ = get_version()
+except LookupError:
+    # Fallback to a default version if SCM metadata is not available
+    __version__ = "0.0.0"
+    
 from urllib.parse import urlparse, quote
 from .download_file import download_file
 from .get_dataset_resource import get_dataset_resource
