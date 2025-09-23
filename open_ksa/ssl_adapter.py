2727823a2620c57ded5ba6c68aa0b5c26579bf76
Esturban <info@evadvisory.ca>
2025-09-23 11:08:56 +0300

    lint: black clean up of format and structure

--- open_ksa/ssl_adapter.py
+++ open_ksa/ssl_adapter.py
@@ -4,16 +4,19 @@ from urllib3 import poolmanager
 from requests.adapters import HTTPAdapter
 import requests
 
+
 class SSLAdapter(HTTPAdapter):
     """
     An HTTPS Transport Adapter that enables SSL connections
     """
+
     def init_poolmanager(self, *args, **kwargs):
         context = ssl.create_default_context()
         if sys.version_info >= (3, 12):
             context.options |= ssl.OP_LEGACY_SERVER_CONNECT
-        kwargs['ssl_context'] = context
+        kwargs["ssl_context"] = context
         return super(SSLAdapter, self).init_poolmanager(*args, **kwargs)
+
 
 class SingletonSession:
     _instance = None
@@ -22,5 +25,5 @@ class SingletonSession:
     def get_instance(cls):
         if cls._instance is None:
             cls._instance = requests.Session()
-            cls._instance.mount('https://', SSLAdapter())
+            cls._instance.mount("https://", SSLAdapter())
         return cls._instance
